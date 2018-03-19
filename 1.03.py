c7f4eaff788e97ecc6c4a4dbe3a1c89bb7f44fa2
azucat2 <1maedakeiji@gmail.com>
2018-03-19 12:34:54 +0900

    add 1.03

---
+++ 1.03.py
@@ -0,0 +1,7 @@
+#-*- coding: utf-8 -*-
+
+str = 'Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics'
+words = str.split(' ')
+
+for word in words:
+    print(len(word), end = '')
