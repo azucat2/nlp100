9f4c434d0d66e122db0a11cb17ad284725ddb4f3
azucat2 <1maedakeiji@gmail.com>
2018-03-19 14:01:31 +0900

    fix 1.03

--- 1.03.py
+++ 1.03.py
@@ -1,6 +1,8 @@
 #-*- coding: utf-8 -*-
 
-str = 'Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics'
+str = 'Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics.'
+str.replace(',', '')
+str.replace('.', '')
 words = str.split(' ')
 
 for word in words:
